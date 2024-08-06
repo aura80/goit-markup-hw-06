a18a0c10bcfe5a7fddae2ffa685cd4f15cc4698a
Aura Dragan <96388713+aura80@users.noreply.github.com>
2024-08-06 20:01:37 +0300

    Initial commit

---
+++ index.js
@@ -0,0 +1,22 @@
+const button = document.querySelector(".btn-modal");
+const backdrop = document.querySelector(".modal-backdrop");
+
+// button.addEventListener("click", () => {
+//     backdrop.classList.add("is-open");
+// });
+
+button.addEventListener("click", toggleModal);
+
+backdrop.addEventListener("click", (e) => {
+    const target = e.target.closest(".btn-close-modal");
+
+    if (!target) {
+        return;
+    }
+
+    toggleModal();
+});
+
+function toggleModal() {
+    backdrop.classList.toggle("is-open");
+}
